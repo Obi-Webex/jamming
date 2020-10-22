0ca231ecae87271ba8ba61be350666e4cf676340
Obi <obiadrian@gmail.com>
2020-10-22 10:50:04 +0300

    Updates

---
+++ precache-manifest.dc5b32b15b2821c61590bca4a582d1aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43992431ac9947544da068258a07228",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "67a10b011be0e456f5c1",
+    "url": "/jamming/static/css/main.ae27161c.chunk.css"
+  },
+  {
+    "revision": "252e9f4e82af0b922ad3",
+    "url": "/jamming/static/js/2.4509aa62.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/jamming/static/js/2.4509aa62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a10b011be0e456f5c1",
+    "url": "/jamming/static/js/main.8beb7619.chunk.js"
+  },
+  {
+    "revision": "d466fbb093b3649303f8",
+    "url": "/jamming/static/js/runtime-main.6d782cb8.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
